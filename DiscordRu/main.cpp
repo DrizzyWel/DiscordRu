0a113825c13f1df05ab39da220f7acdfdaa8ccd6
Айнур Афтахов <drizzywel@mail.ru>
2024-10-12 23:45:03 +0300

    промежуток

--- DiscordRu/main.cpp
+++ DiscordRu/main.cpp
@@ -6,14 +6,14 @@
 using namespace std;
 #pragma comment(linker, "/entry:WinMainCRTStartup")
 #pragma comment(lib,"d3d9.lib")
+#define D3DFVF_CUSTOMVERTEX ( D3DFVF_XYZ | D3DFVF_DIFFUSE )
 struct CUSTOMVERTEX
 {
 	float X, Y, Z; //Координаты в трехмерном пространстве
-	float rhw;//параметр преобразования вершины
 	DWORD color;//цвет
 	 
 };
-#define D3DFVF_CUSTOMVERTEX ( D3DFVF_XYZRHW | D3DFVF_DIFFUSE )
+
 LPDIRECT3D9	pDirect3D = NULL;
 LPDIRECT3DDEVICE9 pDirect3DDevice = NULL;
 LPDIRECT3DVERTEXBUFFER9 pBufferVershin = NULL;
@@ -38,14 +38,34 @@ HRESULT InitialDirect3D(HWND hWnd)
 	Direct3DParametr.BackBufferFormat = Display.Format;
 if(FAILED(	pDirect3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &Direct3DParametr, &pDirect3DDevice)))
 	return E_FAIL;
+pDirect3DDevice->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
+pDirect3DDevice->SetRenderState(D3DRS_LIGHTING, FALSE);
 
+
+}
+void Matrix()
+{
+	D3DXMATRIX MatrixWorld;
+	D3DXMATRIX MatrixView;
+	D3DXMATRIX MatrixProjection;
+	UINT Time = timeGetTime() % 5000;
+	float angle = Time * (2.0f * D3DX_PI) / 5000.0f;
+	D3DXMatrixRotationX(&MatrixWorld, angle);
+	pDirect3DDevice->SetTransform(D3DTS_WORLD, &MatrixWorld);
+	D3DXMatrixLookAtLH(&MatrixView,// -результат выполнения функции
+			new D3DXVECTOR3(0.0f, 0.0f, -8.0f), //-точка, в которой находится камера
+			new D3DXVECTOR3(0.0f, 0.0f, 0.0f), //-точка, в которую мы смотрим
+			new D3DXVECTOR3(0.0f, 1.0f, 0.0f));// -верх объекта
+	pDirect3DDevice->SetTransform(D3DTS_VIEW, &MatrixView);
+	D3DXMatrixPerspectiveFovLH(&MatrixProjection, D3DX_PI / 4, 1.0f, 1.0f, 100.0f);
+	pDirect3DDevice->SetTransform(D3DTS_PROJECTION, &MatrixProjection);
 }
 void RenderingDirect3D()
 {
 	if (pDirect3DDevice == 0)return;
 	pDirect3DDevice->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_ARGB(50,255, 0, 255), 1.0f, 0);
 	pDirect3DDevice->BeginScene();
-
+	Matrix();
 	pDirect3DDevice->SetStreamSource(0, pBufferVershin, 0, sizeof(CUSTOMVERTEX));
 	pDirect3DDevice->SetFVF(D3DFVF_CUSTOMVERTEX);
 
@@ -66,13 +86,13 @@ HRESULT InitialBufferVershin()
 {
 	CUSTOMVERTEX Vershin[] =
 	{
-	{ 300.0f, 300.0f, 0.5f, 1.0f, 0x00000fff },
-	 { 150.0f, 300.0f, 0.5f, 1.0f, 0x00000fff },
-	{150.0f, 150.0f, 0.5f, 1.0f, 0x00000fff },
+	{ 1.0f, -1.0f, 0.0f,  0x00000fff },
+	{ -1.0f, -1.0f, 0.0f,  0x00000fff },
+{ -1.0f, 1.0f, 0.0f,  0x00000fff },
 
-	{ 150.0f, 150.0f, 0.5f, 1.0f, 0x00000fff },
-	 { 300.0f, 150.0f, 0.5f, 1.0f, 0x00000fff },
-	{300.0f, 300.0f, 0.5f, 1.0f, 0x00000fff },
+{ -1.0f, 1.0f, 0.0f,  0x00000fff },
+{ 1.0f, 1.0f, 0.0f,  0x00000fff },
+{ 1.0f, -1.0f, 0.0f,  0x00000fff },
 	};
 	if (FAILED(pDirect3DDevice->CreateVertexBuffer(6 * sizeof(CUSTOMVERTEX), 0, D3DFVF_CUSTOMVERTEX, D3DPOOL_DEFAULT, &pBufferVershin, NULL)))
 		return E_FAIL;
@@ -83,6 +103,7 @@ HRESULT InitialBufferVershin()
 	pBufferVershin->Unlock();
 	return S_OK;
 }
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
